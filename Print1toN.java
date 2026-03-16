6c9fefbe931c62d6829ce8be808fc990375e3086
Ambatipudi VGSD Sai Kalyan <a.saikalyan21@gmail.com>
2026-03-16 15:28:15 +0530

    Add files via upload

---
+++ Print1toN.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Print1toN {
+	
+	public static void main(String args[])
+	
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the Number: ");
+		
+		int n = sc.nextInt();
+		
+		for (int i = 0; i <= n; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
+
