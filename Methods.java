306d762cb95c68dec92a26645fc4896fcc25c4da
Ambatipudi VGSD Sai Kalyan <a.saikalyan21@gmail.com>
2026-03-26 13:21:38 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+
+    static void square(int num)
+    {
+        System.out.println(num * num);
+    }
+    public static void main(String a[])
+    {
+        square(5);
+    }
+}
